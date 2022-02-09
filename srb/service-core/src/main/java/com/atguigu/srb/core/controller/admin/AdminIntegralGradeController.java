4f4e074e3165f0be41c7ddab10dd8310a8faae2a
integer <h_1029384756@163.com>
2022-02-09 16:05:08 +0800

    统一返回结果（续）

--- srb/service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
+++ srb/service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -37,4 +37,44 @@ public class AdminIntegralGradeController {
             return R.error().message("删除失败");
         }
     }
+
+    @ApiOperation("新增积分等级")
+    @PostMapping("/save")
+    public R save(
+            @ApiParam(value = "积分等级对象", required = true)
+            @RequestBody IntegralGrade integralGrade){
+        boolean result = integralGradeService.save(integralGrade);
+        if (result) {
+            return R.ok().message("保存成功");
+        } else {
+            return R.error().message("保存失败");
+        }
+    }
+
+    @ApiOperation("根据id获取积分等级")
+    @GetMapping("/get/{id}")
+    public R getById(
+            @ApiParam(value = "数据id", required = true, example = "1")
+            @PathVariable Long id
+    ){
+        IntegralGrade integralGrade = integralGradeService.getById(id);
+        if(integralGrade != null){
+            return R.ok().data("record", integralGrade);
+        }else{
+            return R.error().message("数据不存在");
+        }
+    }
+
+    @ApiOperation("更新积分等级")
+    @PutMapping("/update")
+    public R updateById(
+            @ApiParam(value = "积分等级对象", required = true)
+            @RequestBody IntegralGrade integralGrade){
+        boolean result = integralGradeService.updateById(integralGrade);
+        if(result){
+            return R.ok().message("修改成功");
+        }else{
+            return R.error().message("修改失败");
+        }
+    }
 }
